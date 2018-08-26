ee68186db9bab8b62e570b84a61fb1d36d7bd7ba
Marcus <wusuzhi@xunlei.com>
2018-08-26 13:25:34 +0800

    project init

---
+++ TokenPocket/TokenPocket/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TokenPocket
+//
+//  Created by MarcusWoo on 2018/8/26.
+//  Copyright © 2018 TokenPocket. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TokenPocket/TokenPocket/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TokenPocket
+//
+//  Created by MarcusWoo on 2018/8/26.
+//  Copyright © 2018 TokenPocket. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
